d6dd08315409a2ecd8f31e9341e223c31e175f9a
Jonas <jonas@deltadevs.xyz>
2025-03-17 14:07:21 +0100

    feat: random KeyBundles can be created

--- PrismMessenger/Crypto/Keys.swift
+++ PrismMessenger/Crypto/Keys.swift
@@ -5,8 +5,8 @@
 //  Copyright © 2025 prism. All rights reserved.
 //
 
-import Foundation
 import CryptoKit
+import Foundation
 
 struct PrivatePrekey: Codable {
     var key_idx: UInt64
@@ -16,7 +16,7 @@ struct PrivatePrekey: Codable {
 struct Prekey: Codable {
     var key_idx: UInt64
     var key: P256.Signing.PublicKey
-    
+
     func fromPrivatePrekey(_ prekey: PrivatePrekey) throws -> Prekey {
         try Prekey(key_idx: prekey.key_idx, key: prekey.key.toP256PrivateKey().publicKey)
     }
@@ -27,4 +27,25 @@ struct KeyBundle: Codable {
     var signed_prekey: P256.Signing.PublicKey
     var signed_prekey_signature: P256.Signing.ECDSASignature
     var prekeys: [Prekey]
+}
+
+// MARK: - Preview
+
+extension KeyBundle {
+    static func random() throws -> Self {
+        let identityKeyPriv = P256.Signing.PrivateKey()
+
+        let signedPrekeyPriv = P256.Signing.PrivateKey()
+        let signedPrekeySignature = try identityKeyPriv.signature(for: signedPrekeyPriv.publicKey.derRepresentation)
+
+        let prekeys = Array(repeating: P256.Signing.PrivateKey(), count: 10)
+            .enumerated()
+            .map { Prekey(key_idx: UInt64($0.offset), key: $0.element.publicKey) }
+
+        return Self(
+            identity_key: identityKeyPriv.publicKey,
+            signed_prekey: signedPrekeyPriv.publicKey,
+            signed_prekey_signature: signedPrekeySignature,
+            prekeys: prekeys)
+    }
 }
