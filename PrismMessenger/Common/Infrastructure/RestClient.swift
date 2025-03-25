ec2a3bea9376405b4b0962c272e2239b0525b3bb
Jonas <jonas@deltadevs.xyz>
2025-03-25 14:40:06 +0100

    feat(common): client sets auth credentials based on current user

--- PrismMessenger/Common/Infrastructure/RestClient.swift
+++ PrismMessenger/Common/Infrastructure/RestClient.swift
@@ -13,13 +13,20 @@ enum RestClientError: Error {
     case invalidURL
     case serdeFailed(String)
     case httpError(Int)
+    case authenticationRequired
     case unknown
+}
+
+enum RestAccessLevel {
+    case pub
+    case authenticated
 }
 
 class RestClient {
 
     private let session: URLSession
     private let baseURL: URL
+    private let userService: UserService
 
     private lazy var encoder: JSONEncoder = {
         let encoder = JSONEncoder()
@@ -33,20 +40,43 @@ class RestClient {
         return decoder
     }()
 
-    init(baseURLStr: String = "http://127.0.0.1:8080") throws {
+    private func authForCurrentUser() async throws -> RestAuthMethod {
+        guard let user = try await userService.getCurrentUser() else {
+            throw RestClientError.authenticationRequired
+        }
+
+        return .basic(username: user.username, password: user.authPassword)
+    }
+
+    private func authMethod(for accessLevel: RestAccessLevel) async throws -> RestAuthMethod {
+        switch accessLevel {
+        case .pub:
+            return .none
+        case .authenticated:
+            return try await authForCurrentUser()
+        }
+    }
+
+    init(baseURLStr: String = "http://127.0.0.1:8080", userService: UserService) throws {
         guard let baseURL = URL(string: baseURLStr) else {
             throw RestClientError.invalidURL
         }
         self.baseURL = baseURL
+        self.userService = userService
 
         let config = URLSessionConfiguration.default
         config.timeoutIntervalForRequest = 30
         self.session = URLSession(configuration: config)
     }
 
-    func head(from path: String) async throws -> HTTPURLResponse {
+    func head(from path: String, accessLevel: RestAccessLevel = .pub) async throws
+        -> HTTPURLResponse
+    {
         var request = URLRequest(url: baseURL.appendingPathComponent(path))
         request.httpMethod = "HEAD"
+
+        let authMethod = try await authMethod(for: accessLevel)
+        try request.applyAuth(authMethod)
 
         let (_, response) = try await session.data(for: request)
 
@@ -57,10 +87,17 @@ class RestClient {
         return httpResponse
     }
 
-    func fetch<T: Decodable>(from path: String) async throws -> T {
-        print(path)
+    func fetch<T: Decodable>(from path: String, accessLevel: RestAccessLevel = .pub) async throws
+        -> T
+    {
         var request = URLRequest(url: baseURL.appendingPathComponent(path))
         request.httpMethod = "GET"
+
+        // Apply authentication method to the request
+        let authMethod = try await authMethod(for: accessLevel)
+        try request.applyAuth(authMethod)
+
+        log.debug("\(path): \(String(describing: request.allHTTPHeaderFields))")
 
         let (data, response) = try await session.data(for: request)
         guard let httpResponse = response as? HTTPURLResponse else {
@@ -83,17 +120,36 @@ class RestClient {
         }
     }
 
-    func post<T: Encodable>(_ data: T, to path: String) async throws {
-        let _ = try await self.postForData(data, to: path)
+    func post<T: Encodable>(_ data: T, to path: String, accessLevel: RestAccessLevel = .pub)
+        async throws
+    {
+        let authMethod = try await authMethod(for: accessLevel)
+        try await self.post(data, to: path, authMethod: authMethod)
     }
 
-    func post<T: Encodable, U: Decodable>(_ data: T, to path: String)
+    func post<T: Encodable>(_ data: T, to path: String, authMethod: RestAuthMethod)
+        async throws
+    {
+        let _ = try await self.postForData(data, to: path, authMethod: authMethod)
+    }
+
+    func post<T: Encodable, U: Decodable>(
+        _ data: T, to path: String, accessLevel: RestAccessLevel = .pub
+    )
         async throws -> U
     {
-        let responseData = try await self.postForData(data, to: path)
+        let authMethod = try await authMethod(for: accessLevel)
+        return try await self.post(data, to: path, authMethod: authMethod)
+    }
+
+    func post<T: Encodable, U: Decodable>(
+        _ data: T, to path: String, authMethod: RestAuthMethod
+    )
+        async throws -> U
+    {
+        let responseData = try await self.postForData(data, to: path, authMethod: authMethod)
         do {
             return try decoder.decode(U.self, from: responseData)
-
         } catch DecodingError.dataCorrupted(let context),
             DecodingError.valueNotFound(_, let context),
             DecodingError.typeMismatch(_, let context),
@@ -103,7 +159,9 @@ class RestClient {
         }
     }
 
-    private func postForData<T: Encodable>(_ data: T, to path: String) async throws
+    private func postForData<T: Encodable>(
+        _ data: T, to path: String, authMethod: RestAuthMethod
+    ) async throws
         -> Data
     {
         let fullURL = baseURL.appendingPathComponent(path)
@@ -111,10 +169,13 @@ class RestClient {
         var request = URLRequest(url: fullURL)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        try request.applyAuth(authMethod)
 
         do {
             request.httpBody = try encoder.encode(data)
-            log.debug("\(path): \(String(data: request.httpBody!, encoding: .utf8) ?? "No body")")
+            log.debug(
+                "\(path): \(String(describing: request.allHTTPHeaderFields)) \(String(data: request.httpBody!, encoding: .utf8) ?? "No body")"
+            )
         } catch EncodingError.invalidValue(_, let context) {
             throw RestClientError.serdeFailed(context.debugDescription)
         }
@@ -128,8 +189,6 @@ class RestClient {
         guard (200...299).contains(httpResponse.statusCode) else {
             throw RestClientError.httpError(httpResponse.statusCode)
         }
-
         return responseData
     }
-
 }
