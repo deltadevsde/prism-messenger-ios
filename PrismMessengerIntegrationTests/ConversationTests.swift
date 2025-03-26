c307c05ef6e1d8bf7117b03babc702ec1889cbee
Jonas <jonas@deltadevs.xyz>
2025-03-26 09:18:22 +0100

    test: extend conversation integration test

--- PrismMessengerIntegrationTests/ConversationTests.swift
+++ PrismMessengerIntegrationTests/ConversationTests.swift
@@ -7,6 +7,7 @@
 
 import Testing
 @testable import PrismMessenger
+import Foundation
 
 final class ConversationTests {
 
@@ -17,15 +18,47 @@ final class ConversationTests {
     }
 
     @Test func twoPeopleChattingWithEachOther() async throws {
-
+        // Simulate two registered users
         try! await appContext.registrationService.registerNewUser(username: "Bob")
         try! await appContext.registrationService.registerNewUser(username: "Alice")
 
-        let chat = try await appContext.chatService.startChat(with: "Bob")
+        // Alice starts a chat with Bob
+        await appContext.userService.selectAccount(username: "Alice")
+        let chatAlice = try await appContext.chatService.startChat(with: "Bob")
+        try await appContext.chatService.sendMessage(content: "Hello Bob", in: chatAlice)
 
-        for i in 0..<20 {
-            try await appContext.chatService.sendMessage(content: "Hello \(i)", in: chat)
+        // Bob receives the message and answers
+        await appContext.userService.selectAccount(username: "Bob")
+        try await appContext.messageService.fetchAndProcessMessages()
+        let chatBob = try await appContext.chatService.getChat(with: "Alice")!
+        try await appContext.chatService.sendMessage(content: "Hello Alice", in: chatBob)
+
+        // Alice receives Bob's answer
+        await appContext.userService.selectAccount(username: "Alice")
+        try await appContext.messageService.fetchAndProcessMessages()
+
+        // repeat multiple times
+        for i in 0..<5 {
+            // Alice sends message to Bob
+            try await appContext.chatService.sendMessage(content: "What up Bob? \(i)", in: chatAlice)
             try await Task.sleep(for: .milliseconds(100))
+
+            // Bob receives and answers
+            await appContext.userService.selectAccount(username: "Bob")
+            try await appContext.messageService.fetchAndProcessMessages()
+            try await appContext.chatService.sendMessage(content: "What up Alice? \(i)", in: chatBob)
+            try await Task.sleep(for: .milliseconds(100))
+
+            // Alice receives
+            await appContext.userService.selectAccount(username: "Alice")
+            try await appContext.messageService.fetchAndProcessMessages()
         }
+
+        // Ensure both chats have 13 messages:
+        // 1 msg for init "Chat established securely ..."
+        // 2 msgs (1 each participant) to start conversation "Hello ..."
+        // 10 msgs (5 each participant) back and forth "What up ..."
+        #expect(chatAlice.messages.count == 13)
+        #expect(chatBob.messages.count == 13)
     }
 }
