018862017079ff306bdbe1dd3ccdb02831dfa6d3
Jonas <jonas@deltadevs.xyz>
2025-03-26 09:15:45 +0100

    feat(messages): more realistic implementation for fake MessageGateway

--- PrismMessenger/Messages/Adapter/Backend/FakeClient+MessageGateway.swift
+++ PrismMessenger/Messages/Adapter/Backend/FakeClient+MessageGateway.swift
@@ -20,37 +20,27 @@ private struct MessageResponse: ReceivedMessage {
     let timestamp = UInt64(Date.now.timeIntervalSince1970 * 1000)
 }
 
-private struct StoredMessage {
-    let messageId: UUID
-    let senderId: String
-    let recipientId: String
-    let message: DoubleRatchetMessage
-}
-
 extension FakeClient: MessageGateway {
 
     func sendMessage(_ message: DoubleRatchetMessage, from sender: String, to recipient: String)
         async throws -> MessageReceipt
     {
-        let storedMessage = StoredMessage(
-            messageId: UUID(),
-            senderId: sender, recipientId: recipient, message: message)
+        let storedMessage = MessageResponse(
+            senderId: sender,
+            recipientId: recipient,
+            message: message)
         store.addToList(storedMessage)
         return SendMessageResponse(messageId: storedMessage.messageId)
     }
 
     func fetchMessages(for username: String) async throws -> [ReceivedMessage] {
-        store.getList(StoredMessage.self)
+        return store.getList(MessageResponse.self)
             .filter { $0.recipientId == username }
-            .map {
-                MessageResponse(
-                    senderId: $0.senderId,
-                    recipientId: $0.recipientId,
-                    message: $0.message)
-            }
     }
 
     func markMessagesAsDelivered(messageIds: [UUID], for username: String) async throws {
-        // do nothing
+        store.removeFromList(MessageResponse.self) {
+            messageIds.contains($0.messageId)
+        }
     }
 }
