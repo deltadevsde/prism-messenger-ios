7963fe02a77fe67827a9f1a54056ec59bfabe1a4
Jonas <jonas@deltadevs.xyz>
2025-05-21 15:10:57 +0200

    feat(profiles): add REST client implementations for profile gateways

--- PrismMessenger/Profiles/Adapter/RestClient+ProfileGateway.swift
+++ PrismMessenger/Profiles/Adapter/RestClient+ProfileGateway.swift
@@ -12,34 +12,68 @@ struct ProfileResponse: Decodable {
     var accountId: UUID
     /// Username of the associated account
     var username: String
+
+    var displayName: String?
+
+    var picture: String?
+}
+
+extension ProfileResponse {
+    func toProfile() -> Profile {
+        return Profile(
+            accountId: self.accountId,
+            username: self.username,
+            displayName: self.displayName,
+            picture: self.picture
+        )
+    }
 }
 
 extension RestClient: ProfileGateway {
+
     func fetchProfile(byAccountId accountId: UUID) async throws -> Profile? {
         do {
             let response: ProfileResponse = try await fetch(
-                from: "/profile/\(accountId.uuidString)", accessLevel: .authenticated)
+                from: "/profile/\(accountId.uuidString)",
+                accessLevel: .authenticated
+            )
 
-            return Profile(accountId: response.accountId, username: response.username)
+            return response.toProfile()
         } catch RestClientError.httpError(let statusCode) {
             if statusCode == 404 {
                 return nil
             }
-            throw UserGatewayError.requestFailed(statusCode)
+            throw ProfileGatewayError.requestFailed(statusCode)
         }
     }
 
     func fetchProfile(byUsername username: String) async throws -> Profile? {
         do {
             let response: ProfileResponse = try await fetch(
-                from: "/profile/by-username/\(username)", accessLevel: .authenticated)
+                from: "/profile/by-username/\(username)",
+                accessLevel: .authenticated
+            )
 
-            return Profile(accountId: response.accountId, username: response.username)
+            return response.toProfile()
         } catch RestClientError.httpError(let statusCode) {
             if statusCode == 404 {
                 return nil
             }
-            throw UserGatewayError.requestFailed(statusCode)
+            throw ProfileGatewayError.requestFailed(statusCode)
+        }
+    }
+
+    func updateProfile(_ request: UpdateProfileRequest) async throws
+        -> ProfilePictureUploadResponse?
+    {
+        do {
+            return try await patch(
+                request,
+                to: "/profile",
+                accessLevel: .authenticated
+            )
+        } catch RestClientError.httpError(let statusCode) {
+            throw ProfileGatewayError.requestFailed(statusCode)
         }
     }
 }
