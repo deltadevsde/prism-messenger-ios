bcf14cdd6ff171f575f562ca1d6e736c8ebfbb80
Jonas <jonas@deltadevs.xyz>
2025-05-21 15:11:02 +0200

    feat(profiles): add fake client implementations for testing

--- PrismMessenger/Profiles/Adapter/FakeClient+ProfileGateway.swift
+++ PrismMessenger/Profiles/Adapter/FakeClient+ProfileGateway.swift
@@ -15,4 +15,39 @@ extension FakeClient: ProfileGateway {
     func fetchProfile(byUsername username: String) async throws -> Profile? {
         store.firstInList(Profile.self) { $0.username == username }
     }
+
+    func updateProfile(_ request: UpdateProfileRequest) async throws
+        -> ProfilePictureUploadResponse?
+    {
+        guard let user = userService.currentUser else {
+            throw FakeClientError.authenticationRequired
+        }
+
+        guard let profile = store.firstInList(Profile.self, where: { $0.accountId == user.id })
+        else {
+            throw ProfileGatewayError.requestFailed(404)
+        }
+
+        if let newDisplayName = request.displayName {
+            profile.displayName = newDisplayName
+        }
+
+        switch request.profilePictureAction {
+        case .clear:
+            profile.picture = nil
+            return nil
+        case .noChange:
+            return nil
+        case .update:
+            profile.picture = nil
+            return ProfilePictureUploadResponse(
+                uploadUrl: "http://some-upload-url.localhost",
+                pictureUrl: URL(string: "http://some-picture-url.localhost")!.appending(
+                    path: "/profiles"
+                )
+                .absoluteString,
+                expiresIn: 300
+            )
+        }
+    }
 }
